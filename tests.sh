8915747a5fa4c243e2e23ee68a96c09c8f9cf97d
Chris Joakim <christopher.joakim@gmail.com>
2015-10-11 19:15:42 -0400

    tests.sh updated; including only m26 in coverage.

--- tests.sh
+++ tests.sh
@@ -4,4 +4,4 @@ echo 'running all tests with nose and code coverage...'
 
 source bin/activate
 
-nosetests --with-coverage --cover-html --cover-html-dir=coverage
+nosetests --with-coverage --cover-html --cover-html-dir=coverage --cover-package=m26

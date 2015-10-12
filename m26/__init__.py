ce24a71d92e39f6a877d6b9c68543d83599b514a
Chris Joakim <christopher.joakim@gmail.com>
2015-10-12 09:00:47 -0400

    Alpha 0.0.3 released, starting 0.0.4

--- m26/__init__.py
+++ m26/__init__.py
@@ -1,5 +1,5 @@
 __author__  = 'cjoakim'
-__version__ = '0.0.3'
+__version__ = '0.0.4'
 
 VERSION = __version__
 
--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     name='m26',
 
     # Versions should comply with PEP440.
-    version='0.0.3',
+    version='0.0.4',
 
     description='m26 is a python library for speed and pace calculations for sports like running and cycling',
     long_description=long_description,

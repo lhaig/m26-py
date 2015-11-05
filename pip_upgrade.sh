d2a74bd9f260e2eb4bcc0be0b5623ba89587d925
Chris Joakim <christopher.joakim@gmail.com>
2015-11-05 15:25:56 -0500

    Converting project to use pip-tools and requirements.in

--- pip_upgrade.sh
+++ pip_upgrade.sh
@@ -1,24 +1,17 @@
 #!/bin/bash
 
 # Shell script to install or upgrade python packages for this pyvenv.
-# Chris Joakim, 2015/10/18
+# Chris Joakim, 2015/11/05
 
 source bin/activate
 
-echo 'upgrading pip and libraries used by this project...'
-
 pip install --upgrade pip
+pip install --upgrade pip-tools
 
-pip3 --version
-python3 --version
+pip-compile requirements.in
+pip install -r requirements.txt
+pip-sync
 
-pip install --upgrade arrow
-pip install --upgrade check-manifest
-pip install --upgrade coverage
-pip install --upgrade flake8
-pip install --upgrade nose
-
-pip list   > pip_list.txt
-pip freeze > requirements.txt
+pip list > pip_list.txt
 
 echo 'done'

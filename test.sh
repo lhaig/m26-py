06dc9fcd726db6f7cefc6d264501e80c49ded1f6
Chris Joakim <christopher.joakim@gmail.com>
2017-09-28 05:25:24 -0400

    Removed build.py and refactored __init__.py to reference m26 modules.

--- test.sh
+++ test.sh
@@ -2,11 +2,8 @@
 
 source bin/activate
 
-echo 'building/merging the codebase...'
-python build.py
-
-echo 'checking the merged source code with flake8...'
-flake8 m26/__init__.py
+echo 'checking the source code with flake8 ...'
+flake8 m26 --ignore F401
 
 echo 'running all tests with nose and code coverage...'
 nosetests --with-coverage --cover-html --cover-html-dir=coverage --cover-package=m26

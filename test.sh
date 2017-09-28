b5917c213e0647427a5ba2d8afebc0e34c116859
Chris Joakim <christopher.joakim@gmail.com>
2017-09-28 05:36:15 -0400

    Migrating from nose to pytest - wip

--- test.sh
+++ test.sh
@@ -5,5 +5,13 @@ source bin/activate
 echo 'checking the source code with flake8 ...'
 flake8 m26 --ignore F401
 
-echo 'running all tests with nose and code coverage...'
-nosetests --with-coverage --cover-html --cover-html-dir=coverage --cover-package=m26
+echo 'executing unit tests with code coverage ...'
+pytest -v --cov=m26/ --cov-report html tests/
+
+# For sdist deployment to PyPi, or local PyPi server:
+# python setup.py sdist
+# python setup.py sdist upload
+# python setup.py sdist upload -r local
+# python setup.py sdist upload -r pypilegacy
+# ls -al /Users/cjoakim/pypi-packages
+# rm /Users/cjoakim/pypi-packages/m26*
